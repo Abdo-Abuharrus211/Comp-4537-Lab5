4f5d9babbbbc32154b9899be8c85523e3ea87702
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-02-15 15:41:14 -0800

    Add method to handle unallowed req methods

--- server2/app.js
+++ server2/app.js
@@ -1,27 +1,32 @@
 const http = require("http");
 const url = require('url');
 
-class QueryHandler{
+class QueryHandler {
 
-    constructor(port){
+    constructor(port) {
         this.port = port;
         this.server = http.createServer(this.handleRequest.bind(this));
     }
 
-    handleRequest(req, res){
+    handleRequest(req, res) {
         const parsedUrl = url.parse(req.url, true);
         const pathname = parsedUrl.pathname;
 
-        if(req.method === 'GET'){
+        if (req.method === 'GET') {
             //Do stuff here
             // validate and run the SQL query by passing it to DB?
         }
-        else if(req.method === 'POST'){
+        else if (req.method === 'POST') {
             // validate and run the SQL query by passing it to DB?
         }
-        else{
+        else {
             this.handleMethodNotAllowed(res);
         }
+    }
+
+    handleMethodNotAllowed(res) {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end("Method Not Allowed");
     }
 
 }
